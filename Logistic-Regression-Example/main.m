8cd67f1b85bde65276e1e0e34a5d818c276848e7
Kordian D <kordiandrozd@gmail.com>
2017-02-06 18:42:46 +0100

    Logistic Regression example added

---
+++ Logistic-Regression-Example/main.m
@@ -0,0 +1,7 @@
+%We have some data, we want to predict based on logistic regression
+%Load data
+load classification.csv
+
+%Calculates number of values
+numberOfValues = length(classification(:,1));
+
